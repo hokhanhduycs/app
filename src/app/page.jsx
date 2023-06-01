2e1da1a9c6782ab7a4ef9e24aecf0ce46eca1c4c
hokhanhduycs <hokhanhduycs@gmail.com>
2023-06-01 18:08:34 +0700

    edit home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,21 @@
 import Image from 'next/image'
-import './page.module.css'
+import style from './page.module.scss'
+import Hero from "public/hero.png"
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+     <div className={style.home}>
+        <div className={style.text}>
+           <h1 className={style.title}>
+              Compiled client and server successfully in 1088 ms.
+           </h1>
+           <p className={style.desc}>
+              Warn Fast Refresh had to perform a full reload when
+              src\app\page.module.scss.
+           </p>
+           <button className={style.button}>See Our Word</button>
+        </div>
+        <Image className={style.img} src={Hero} alt="image home" />
+     </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,10 +10,34 @@ const Footer = () => {
             Copyright © 2023 Khanh Duy. All rights reserved.
          </div>
          <div className={style.social}>
-            <Image src="/1.png" width={15} height={15} className={style.icon} alt="fb" />
-            <Image src="/2.png" width={15} height={15} className={style.icon} alt="ig" />
-            <Image src="/3.png" width={15} height={15} className={style.icon} alt="w" />
-            <Image src="/4.png" width={15} height={15} className={style.icon} alt="yb" />
+            <Image
+               src="/1.png"
+               width={15}
+               height={15}
+               className={style.icon}
+               alt="fb"
+            />
+            <Image
+               src="/2.png"
+               width={15}
+               height={15}
+               className={style.icon}
+               alt="ig"
+            />
+            <Image
+               src="/3.png"
+               width={15}
+               height={15}
+               className={style.icon}
+               alt="w"
+            />
+            <Image
+               src="/4.png"
+               width={15}
+               height={15}
+               className={style.icon}
+               alt="yb"
+            />
          </div>
       </div>
    );
